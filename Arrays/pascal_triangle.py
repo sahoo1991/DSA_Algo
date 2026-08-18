31ff080b152de77ac15f312e3cb87cbe68cefad5
SangrambadiSahoo <sahoosbautoamtion@gmail.com>
2026-08-18 15:14:51 +0530

    pascal triangel all possible

--- Arrays/pascal_triangle.py
+++ Arrays/pascal_triangle.py
@@ -7,8 +7,8 @@ def pascal_triangle(n):
             current.append(prev[j] + prev[j+1])
         current.append(1)
         result.append(current)
-    for item in result:
-        print(item)
+    # for item in result:
+    #     print(item)
     return result
 
 def get_pascal_triangle_value(n, r):
@@ -23,10 +23,22 @@ def get_pascal_triangle_value(n, r):
     return result
 
 
+def get_pascal_triangle_row(n):
+    row = [1]
+    for i in range(1, n):
+        row.append(row[i-1] * (n-i) // i)
+    return row
 
 
+def get_pascal_triangle_optimized(n):
+    result =  [[1]]
+    for i in range(2, n + 1):
+        result.append(get_pascal_triangle_row(i))
+    return result
 
 
 n = 5
-result = pascal_triangle(n)
-print(get_pascal_triangle_value(4,4))
+# result = pascal_triangle(n)
+# print(get_pascal_triangle_value(4,4))
+print(get_pascal_triangle_row(5))
+print(get_pascal_triangle_optimized(5))
